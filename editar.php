3c6712407e63260ac147783e70c0d7584c946260
davidfainy24 <davidarmandofainy@gmail.com>
2026-05-06 19:01:40 -0500

    cambio3

--- editar.php
+++
@@ -1,9 +0,0 @@
-<?php
-include 'db.php';
-
-// 1. Cargar los datos actuales del artículo
-if (isset($_GET['id'])) {
-    $id = $_GET['id'];
-    $resultado = mysqli_query($conn, "SELECT * FROM articulos WHERE id = $id");
-    $articulo = mysqli_fetch_assoc($resultado);
-}
