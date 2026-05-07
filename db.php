679db79be50f3348992a47e18e7bfc03bbd4bdee
davidfainy24 <davidarmandofainy@gmail.com>
2026-05-06 19:13:27 -0500

    cambio7

--- db.php
+++ db.php
@@ -1,4 +1,4 @@
 <?php
-$conn = mysqli_connect("localhost", "root", "", "gestion_inventario");
+$conn = mysqli_connect("mysql-duberfainy.alwaysdata.net", "duberfainy", "clase1234", "duberfainy_gestionarticulos");
 if (!$conn) { die("Error de conexión: " . mysqli_connect_error()); }
 ?>
